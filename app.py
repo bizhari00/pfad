b277d7cb9a8b1a6fccf3d90169f4fd38310da792
bizhari00 <bustamiizhari@gmail.com>
2026-05-16 15:20:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ flow_path = [
     {
         'step_id': 'biodiesel',
         'x': 900, 'y': y_arrow, 'label': 'Produk Biodiesel', 
-        'tank_area': [1150, 400, 1200, 530]
+        'tank_area': [1150, 400, 1250, 530]
     }
 ]
 
